fa9d115e1b3b5790adf1f3dc77013fa84e6df1f0
ilm4zz4 <michele.rosellini@gmail.com>
2015-02-22 22:23:23 +0100

    add unit test

---
+++ test/src/sample1_unittest.cc
@@ -0,0 +1,14 @@
+
+#include <limits.h>
+#include "dhcp_server.h"
+#include "gtest/gtest.h"
+
+
+// Tests factorial of negative numbers.
+TEST(ip_asc2bytes, prova) {
+  // This test is named "Negative", and belongs to the "FactorialTest"
+  // test case.
+  EXPECT_EQ(1, ip_asc2bytes(NULL, NULL));
+}
+
+
