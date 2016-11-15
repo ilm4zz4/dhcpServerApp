84625497cfda823d366a6961c65ca6182d03d351
ilmazza <mrosellini@navionics.com>
2016-11-15 21:03:36 +0100

    Add initDB function

--- src/ip_allocator_sql.c
+++ src/ip_allocator_sql.c
@@ -7,6 +7,52 @@
 
 extern struct server_config gobal_config;
 
+int initDB(){
+	
+	INFO("==>sqlite_ip_allocate");
+	sqlite3 *db = NULL;
+
+	int ret = sqlite3_open(gobal_config.ip_allocator_file, &db);
+	if(SQLITE_OK != ret)
+        {
+                ERROR("***sqlite3_open ERROR!!! %s(%d)***", sqlite3_errmsg(db), ret);
+                goto ERROR;
+        }
+
+	//If doesn't exist create database table
+	//MAC: mac which send discovery message
+	//IP: ip address which shall be assigned to host
+	//ACTIVE: the ip is already assign
+	//MASK, GW, DNS: configuration client
+    char *sql = "DROP TABLE IF EXISTS Network;" 
+                "CREATE TABLE Network( MAC TEXT, IP TETXT, ACTIVE INT, MASK TEXT, GW TEXT, DNS);" 
+
+    rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+    if (rc != SQLITE_OK ) {
+        
+        fprintf(stderr, "***sqlite3_exec ERROR!!! %s(%d)***", err_msg);
+        
+        sqlite3_free(err_msg);        
+        sqlite3_close(db);
+        
+        return 1;
+    } 
+
+	//Deactivation all ip address
+	
+   sqlite3_stmt *statement = NULL;
+   
+   int ret = sqlite3_prepare(db, "UPDATE Network SET ACTIVE=0 WHERE ACTIVE=1;",128, &statement, NULL);
+   if(SQLITE_OK != ret){
+        fprintf(stderr, "***sqlite3_prepare: get all entryes ERROR!!! %s(%d)***", err_msg);
+    	exit (1);
+   }
+
+	sqlite3_finalize(statement);
+
+    sqlite3_close(db);
+}
+
 int sqlite_ip_allocator(struct network_config *config)
 {
 	INFO("==>sqlite_ip_allocate");
@@ -17,7 +63,7 @@ int sqlite_ip_allocator(struct network_config *config)
         {
                 ERROR("***sqlite3_open ERROR!!! %s(%d)***", sqlite3_errmsg(db), ret);
                 goto ERROR;
-        }
+        }	
 
 	sqlite3_stmt *statement = NULL;
 
@@ -82,7 +128,7 @@ int sqlite_ip_allocator(struct network_config *config)
 
     if(SQLITE_OK != ret)
     {
-    	ERROR("***sqlite3_prepare ERROR!!! %s(%d)***", sqlite3_errmsg(db), ret);
+		ERROR("***sqlite3_prepare ERROR!!! %s(%d)***", sqlite3_errmsg(db), ret);
         goto ERROR;
     }
 	
@@ -90,7 +136,7 @@ int sqlite_ip_allocator(struct network_config *config)
 
     if(SQLITE_ROW != ret)
     {
-    	ERROR("***sqlite3_step ERROR!!! %s(%d)***", sqlite3_errmsg(db), ret);
+		ERROR("***sqlite3_step ERROR!!! %s(%d)***", sqlite3_errmsg(db), ret);
         goto ERROR;
     }
 
