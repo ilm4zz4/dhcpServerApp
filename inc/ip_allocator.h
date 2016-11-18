d11581d6d36d5ebf2db3d5bfd050f851ede4562d
ilmazza <mrosellini@navionics.com>
2016-11-18 09:09:08 +0100

    fix ip_allocator problem

--- inc/ip_allocator.h
+++ inc/ip_allocator.h
@@ -1,5 +1,15 @@
 #ifndef __IP_ALLOCATOR_H__
 #define __IP_ALLOCATOR_H__
+
+#ifdef __cplusplus /* If this is a C++ compiler, use C linkage */
+extern "C" {
+#endif
 int reset_database();
+
+#ifdef __cplusplus /* If this is a C++ compiler, end C linkage */
+}
+#endif
+
+
 
 #endif
